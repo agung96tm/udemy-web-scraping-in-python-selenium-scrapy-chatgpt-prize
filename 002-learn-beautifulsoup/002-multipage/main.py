f54a12cef333d230f89a7ea584f6fc3152e91927
agunggapai <agung@gapai.id>
2026-04-29 13:02:25 +0700

    BeautifulSoup: Multi Pages

--- 002-learn-beautifulsoup/002-multipage/main.py
+++ 002-learn-beautifulsoup/002-multipage/main.py
@@ -1,16 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-website = "https://subslikescript.com/movie/Titanic-46435"
+base_website = "https://subslikescript.com"
+website = f"{base_website}/movies"
 result = requests.get(website)
 content = result.text
-
 soup = BeautifulSoup(content, "lxml")
 
-
+links = []
 box = soup.find("article", class_="main-article")
-title = box.find("h1").get_text()
-transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
+for link in box.find_all("a", href=True):
+    links.append(link["href"])
 
-with open(f"{title}.txt", "w") as file:
-    file.write(transcript)
+for link in links:
+    website = f"{base_website}/{link}"
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, "lxml")
+
+    box = soup.find("article", class_="main-article")
+
+    title = box.find("h1").get_text()
+    transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
+
+    with open(f"{title}.txt", "w") as file:
+        file.write(transcript)
